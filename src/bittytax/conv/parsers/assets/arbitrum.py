3dda2d6477a8e32d61e2ec20725a6eae52ad6807
farynaio <adam22@lifeisgood.cyou>
2023-01-31 01:30:22 +0000

    Arbitrum: added support

---
+++ src/bittytax/conv/parsers/assets/arbitrum.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+# (c)
+
+from decimal import Decimal
+
+from .etherscan import get_note
+from ..out_record import TransactionOutRecord
+from ..dataparser import DataParser
+
+WALLET = "Arbitrum"
+WORKSHEET_NAME = "ArbiScan"
+
+def parse_arbiscan(data_row, _parser, **_kwargs):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(int(row_dict['UnixTimestamp']))
+
+    if row_dict['Status'] != '':
+        # Failed txns should not have a Value_OUT
+        row_dict['Value_OUT(ETH)'] = 0
+
+    if Decimal(row_dict['Value_IN(ETH)']) > 0:
+        if row_dict['Status'] == '':
+            data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                     data_row.timestamp,
+                                                     buy_quantity=row_dict['Value_IN(ETH)'],
+                                                     buy_asset="ETH",
+                                                     wallet=get_wallet(row_dict['To']),
+                                                     note=get_note(row_dict))
+    elif Decimal(row_dict['Value_OUT(ETH)']) > 0:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
+                                                 data_row.timestamp,
+                                                 sell_quantity=row_dict['Value_OUT(ETH)'],
+                                                 sell_asset="ETH",
+                                                 fee_quantity=row_dict['TxnFee(ETH)'],
+                                                 fee_asset="ETH",
+                                                 wallet=get_wallet(row_dict['From']),
+                                                 note=get_note(row_dict))
+    else:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_SPEND,
+                                                 data_row.timestamp,
+                                                 sell_quantity=row_dict['Value_OUT(ETH)'],
+                                                 sell_asset="ETH",
+                                                 fee_quantity=row_dict['TxnFee(ETH)'],
+                                                 fee_asset="ETH",
+                                                 wallet=get_wallet(row_dict['From']),
+                                                 note=get_note(row_dict))
+
+def get_wallet(address):
+    return "%s-%s" % (WALLET, address.lower()[0:TransactionOutRecord.WALLET_ADDR_LEN])
+
+def parse_arbiscan_internal(data_row, _parser, **_kwargs):
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(int(row_dict['UnixTimestamp']))
+
+    # Failed internal txn
+    if row_dict['Status'] != '0':
+        return
+
+    if Decimal(row_dict['Value_IN(ETH)']) > 0:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                 data_row.timestamp,
+                                                 buy_quantity=row_dict['Value_IN(ETH)'],
+                                                 buy_asset="ETH",
+                                                 wallet=get_wallet(row_dict['TxTo']))
+    elif Decimal(row_dict['Value_OUT(ETH)']) > 0:
+        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
+                                                 data_row.timestamp,
+                                                 sell_quantity=row_dict['Value_OUT(ETH)'],
+                                                 sell_asset="ETH",
+                                                 wallet=get_wallet(row_dict['From']))
+
+arbitrum_txns = DataParser(
+        DataParser.TYPE_EXPLORER,
+        "ArbiScan (Arbitrum Transactions)",
+    ["Txhash","Blockno","UnixTimestamp","DateTime","From","To","ContractAddress","Value_IN(ETH)","Value_OUT(ETH)",None,"TxnFee(ETH)","TxnFee(USD)","Historical $Price/ETH","Status","ErrCode","Method"],
+        worksheet_name=WORKSHEET_NAME,
+        row_handler=parse_arbiscan)
+
+DataParser(DataParser.TYPE_EXPLORER,
+           "ArbiScan (Arbitrum Transactions)",
+           ['Txhash', 'Blockno', 'UnixTimestamp', 'DateTime', 'From', 'To', 'ContractAddress',
+            'Value_IN(ETH)', 'Value_OUT(ETH)', None, 'TxnFee(ETH)', 'TxnFee(USD)',
+            'Historical $Price/ETH', 'Status', 'ErrCode', 'PrivateNote'],
+           worksheet_name=WORKSHEET_NAME,
+           row_handler=parse_arbiscan)
+
+DataParser(DataParser.TYPE_EXPLORER,
+           "ArbiScan (Arbitrum Transactions)",
+           ['Txhash', 'Blockno', 'UnixTimestamp', 'DateTime', 'From', 'To', 'ContractAddress',
+            'Value_IN(ETH)', 'Value_OUT(ETH)', None, 'TxnFee(ETH)', 'TxnFee(USD)',
+            'Historical $Price/ETH', 'Status', 'ErrCode', 'Method'],
+           worksheet_name=WORKSHEET_NAME,
+           row_handler=parse_arbiscan)
+
+DataParser(DataParser.TYPE_EXPLORER,
+           "ArbiScan (Arbitrum Transactions)",
+           ['Txhash', 'Blockno', 'UnixTimestamp', 'DateTime', 'From', 'To', 'ContractAddress',
+            'Value_IN(ETH)', 'Value_OUT(ETH)', None, 'TxnFee(ETH)', 'TxnFee(USD)',
+            'Historical $Price/ETH', 'Status', 'ErrCode', 'Method', 'PrivateNote'],
+           worksheet_name=WORKSHEET_NAME,
+           row_handler=parse_arbiscan)
+
+arbitrum_int = DataParser(
+        DataParser.TYPE_EXPLORER,
+        "ArbiScan (Arbitrum Internal Transactions)",
+        ['Txhash', 'Blockno', 'UnixTimestamp', 'DateTime', 'ParentTxFrom', 'ParentTxTo',
+         'ParentTxETH_Value', 'From', 'TxTo', 'ContractAddress', 'Value_IN(ETH)',
+         'Value_OUT(ETH)', None, 'Historical $Price/ETH', 'Status', 'ErrCode', 'Type'],
+        worksheet_name=WORKSHEET_NAME,
+        row_handler=parse_arbiscan_internal)
+
+DataParser(DataParser.TYPE_EXPLORER,
+           "ArbiScan (Arbitrum Internal Transactions)",
+           ['Txhash', 'Blockno', 'UnixTimestamp', 'DateTime', 'ParentTxFrom', 'ParentTxTo',
+            'ParentTxETH_Value', 'From', 'TxTo', 'ContractAddress', 'Value_IN(ETH)',
+            'Value_OUT(ETH)', None, 'Historical $Price/ETH', 'Status', 'ErrCode', 'Type',
+            'PrivateNote'],
+           worksheet_name=WORKSHEET_NAME,
+           row_handler=parse_arbiscan_internal)
+
+# Same header as Etherscan
+#DataParser(DataParser.TYPE_EXPLORER,
+#           "BscScan (BEP-20 Tokens)",
+#           ['Txhash', 'UnixTimestamp', 'DateTime', 'From', 'To', 'Value', 'ContractAddress',
+#            'TokenName', 'TokenSymbol'],
+#           worksheet_name=WORKSHEET_NAME,
+#           row_handler=parse_bscscan_tokens)
+
+# Same header as Etherscan
+#DataParser(DataParser.TYPE_EXPLORER,
+#           "Etherscan (BEP-721 NFTs)",
+#           ['Txhash', 'UnixTimestamp', 'DateTime', 'From', 'To', 'ContractAddress', 'TokenId',
+#            'TokenName', 'TokenSymbol'],
+#           worksheet_name=WORKSHEET_NAME,
+#           row_handler=parse_bscscan_nfts)
