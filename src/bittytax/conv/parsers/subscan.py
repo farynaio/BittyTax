52eb156cbfcbd49a4ac2ed009bed392c99ed329f
farynaio <adam10@puremail.cyou>
2025-01-28 18:09:40 +0000

    subscan: added crosschain transfers, fixed paidout

--- src/bittytax/conv/parsers/subscan.py
+++ src/bittytax/conv/parsers/subscan.py
@@ -24,6 +24,48 @@ NETWORK_TO_TOKEN = {
 }
 
 
+def parse_subscan_crosschain_transfers(
+    data_row: "DataRow", parser: DataParser, **kwargs: Unpack[ParserArgs]
+) -> None:
+    row_dict = data_row.row_dict
+
+    data_row.timestamp = DataParser.parse_timestamp(row_dict["Time"])
+
+    data_row.tx_raw = TxRawPos(
+        parser.in_header.index("Message Hash"),
+        parser.in_header.index("From Account"),
+        parser.in_header.index("To Account"),
+    )
+
+    if row_dict["Status"].lower() != "success":
+        return
+
+    network = _get_network(kwargs["filename"])
+
+    if row_dict["To Account"].lower() in kwargs["filename"].lower():
+        data_row.t_record = TransactionOutRecord(
+            TrType.DEPOSIT,
+            data_row.timestamp,
+            buy_quantity=Decimal(row_dict["Value"].split(' ')[0]),
+            buy_asset=row_dict["Value"].split(' ')[1],
+            wallet=_get_wallet(network, row_dict["To Account"]),
+            note=_get_note(row_dict)
+        )
+    elif row_dict["From Account"].lower() in kwargs["filename"].lower():
+        data_row.t_record = TransactionOutRecord(
+            TrType.WITHDRAWAL,
+            data_row.timestamp,
+            sell_quantity=Decimal(row_dict["Value"].split(' ')[0]),
+            sell_asset=row_dict["Value"].split(' ')[1],
+            wallet=_get_wallet(network, row_dict["From Account"]),
+            note=_get_note(row_dict)
+        )
+    else:
+        print("address", network)
+        raise DataFilenameError(kwargs["filename"], f"{network} address")
+
+
+
 def parse_subscan_transfers(
     data_row: "DataRow", parser: DataParser, **kwargs: Unpack[ParserArgs]
 ) -> None:
@@ -82,6 +124,23 @@ def _get_wallet(network: str, address: str) -> str:
     return f"{network}-{address.lower()[0 : TransactionOutRecord.WALLET_ADDR_LEN]}"
 
 
+def parse_subscan_paidout_v2(
+    data_row: "DataRow", _parser: DataParser, **kwargs: Unpack[ParserArgs]
+) -> None:
+    row_dict = data_row.row_dict
+    data_row.timestamp = DataParser.parse_timestamp(re.sub(r" \(\+?UTC\)", "", row_dict["Date"]))
+    network = _get_network(kwargs["filename"])
+    token = _get_token(network, **kwargs)
+
+    data_row.t_record = TransactionOutRecord(
+        TrType.STAKING,
+        data_row.timestamp,
+        buy_quantity=Decimal(row_dict["Value"]),
+        buy_asset=token,
+        wallet=_get_wallet(network, row_dict["Reward Account"]),
+        note=_get_note(row_dict)
+    )
+
 def parse_subscan_paidout(
     data_row: "DataRow", _parser: DataParser, **kwargs: Unpack[ParserArgs]
 ) -> None:
@@ -108,6 +167,28 @@ def _get_token(network: str, **kwargs: Unpack[ParserArgs]) -> str:
         return kwargs["cryptoasset"]
     raise UnknownCryptoassetError(kwargs["filename"], kwargs.get("worksheet", ""))
 
+DataParser(
+    ParserType.EXPLORER,
+    "Subscan Crosschain Transfers",
+    [
+        "Message Hash",
+        "From Account",
+        "To Account",
+        "Block",
+        "Origin Event Index",
+        "Dest Event Index",
+        "Time",
+        "From Chain ID",
+        "Destination Chain ID",
+        "Value",
+        "Protocol",
+        "Status"
+    ],
+    worksheet_name="Subscan",
+    row_handler=parse_subscan_crosschain_transfers,
+)
+
+
 
 DataParser(
     ParserType.EXPLORER,
@@ -123,6 +204,14 @@ DataParser(
     ["Extrinsic ID", "Date", "Block", "Hash", "Symbol", "From", "To", "Value", "Result"],
     worksheet_name="Subscan",
     row_handler=parse_subscan_transfers,
+)
+
+DataParser(
+    ParserType.EXPLORER,
+    "Subscan Pool Paidout",
+    ["Event Index","Era","Date","Block","Extrinsic Index","Value","Action","Stash","Reward Account","Validator Stash"],
+    worksheet_name="Subscan",
+    row_handler=parse_subscan_paidout_v2,
 )
 
 DataParser(
